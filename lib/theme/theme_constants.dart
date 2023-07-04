e310670b7eda7ab1234d5c12aee9d1015dde279a
Pham Nguyen <trungnguyengl176@gmail.com>
2023-07-04 23:03:50 +0700

    chore: set up text theme

--- lib/theme/theme_constants.dart
+++ lib/theme/theme_constants.dart
@@ -27,30 +27,30 @@ class AppColors extends ThemeExtension<AppColors> {
     required this.secondaryDark,
   });
 
-  final Color? primaryBase;
-  final Color? primaryLight;
-  final Color? primaryLighter;
-  final Color? primaryLightest;
-  final Color? primaryDark;
+  final Color primaryBase;
+  final Color primaryLight;
+  final Color primaryLighter;
+  final Color primaryLightest;
+  final Color primaryDark;
 
-  final Color? skyBase;
-  final Color? skyLight;
-  final Color? skyLighter;
-  final Color? skyLightest;
-  final Color? skyDark;
+  final Color skyBase;
+  final Color skyLight;
+  final Color skyLighter;
+  final Color skyLightest;
+  final Color skyDark;
 
-  final Color? inkBase;
-  final Color? inkLight;
-  final Color? inkLighter;
-  final Color? inkLightest;
-  final Color? inkDarker;
-  final Color? inkDarkest;
+  final Color inkBase;
+  final Color inkLight;
+  final Color inkLighter;
+  final Color inkLightest;
+  final Color inkDarker;
+  final Color inkDarkest;
 
-  final Color? secondaryBase;
-  final Color? secondaryLight;
-  final Color? secondaryLighter;
-  final Color? secondaryLightest;
-  final Color? secondaryDark;
+  final Color secondaryBase;
+  final Color secondaryLight;
+  final Color secondaryLighter;
+  final Color secondaryLightest;
+  final Color secondaryDark;
 
   @override
   AppColors copyWith(
@@ -106,28 +106,27 @@ class AppColors extends ThemeExtension<AppColors> {
       return this;
     }
     return AppColors(
-      primaryBase: Color.lerp(primaryBase, other.primaryBase, t),
-      primaryLight: Color.lerp(primaryLight, other.primaryLight, t),
-      primaryLighter: Color.lerp(primaryLighter, other.primaryLighter, t),
-      primaryLightest: Color.lerp(primaryLightest, other.primaryLightest, t),
-      primaryDark: Color.lerp(primaryDark, other.primaryDark, t),
-      skyBase: Color.lerp(skyBase, other.skyBase, t),
-      skyLight: Color.lerp(skyLight, other.skyLight, t),
-      skyLighter: Color.lerp(skyLighter, other.skyLighter, t),
-      skyLightest: Color.lerp(skyLightest, other.skyLightest, t),
-      skyDark: Color.lerp(skyDark, other.skyDark, t),
-      inkBase: Color.lerp(inkBase, other.inkBase, t),
-      inkLight: Color.lerp(inkLight, other.inkLight, t),
-      inkLighter: Color.lerp(inkLighter, other.inkLighter, t),
-      inkLightest: Color.lerp(inkLightest, other.inkLightest, t),
-      inkDarker: Color.lerp(inkDarker, other.inkDarker, t),
-      inkDarkest: Color.lerp(inkDarkest, other.inkDarkest, t),
-      secondaryBase: Color.lerp(secondaryBase, other.secondaryBase, t),
-      secondaryLight: Color.lerp(secondaryLight, other.secondaryLight, t),
-      secondaryLighter: Color.lerp(secondaryLighter, other.secondaryLighter, t),
-      secondaryLightest:
-          Color.lerp(secondaryLightest, other.secondaryLightest, t),
-      secondaryDark: Color.lerp(secondaryDark, other.secondaryDark, t),
+      primaryBase: Color(0xFF439A97),
+      primaryLight: Color(0xFF69AEAC),
+      primaryLighter: Color(0xFF8EC2C1),
+      primaryLightest: Color(0xFFD9EBEA),
+      primaryDark: Color(0xFF367B79),
+      skyBase: Color(0xFF439A97),
+      skyLight: Color(0xFF69AEAC),
+      skyLighter: Color(0xFF8EC2C1),
+      skyLightest: Color(0xFFD9EBEA),
+      skyDark: Color(0xFF367B79),
+      secondaryBase: Color(0xFF439A97),
+      secondaryLight: Color(0xFF69AEAC),
+      secondaryLighter: Color(0xFF8EC2C1),
+      secondaryLightest: Color(0xFFD9EBEA),
+      secondaryDark: Color(0xFF367B79),
+      inkBase: Color(0xFF439A97),
+      inkLight: Color(0xFF69AEAC),
+      inkLighter: Color(0xFF8EC2C1),
+      inkLightest: Color(0xFFD9EBEA),
+      inkDarker: Color(0xFF367B79),
+      inkDarkest: Color(0xFF367B79),
     );
   }
 }
@@ -139,46 +138,51 @@ ThemeData lightTheme = ThemeData(
   ),
   textTheme: TextTheme(
     //Heading
-    //Heading 1
     headlineLarge: GoogleFonts.sourceSansPro(
       color: Color.fromARGB(80, 2, 2, 2),
       fontSize: 26.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeight.w600,
     ),
-    //Heading 2
     headlineMedium: GoogleFonts.sourceSansPro(
       color: Color.fromARGB(80, 2, 2, 2),
       fontSize: 20.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeight.w600,
     ),
-    //Heading 3
     headlineSmall: GoogleFonts.sourceSansPro(
       color: Color.fromARGB(252, 2, 2, 2),
       fontSize: 18.0,
-      fontWeight: FontWeight.bold,
+      fontWeight: FontWeight.w600,
     ),
-    //Heading 4
-    displayLarge: GoogleFonts.sourceSansPro(
+    titleLarge: GoogleFonts.sourceSansPro(
         color: Color.fromARGB(252, 2, 2, 2),
         fontSize: 18.0,
-        fontWeight: FontWeight.bold,
+        fontWeight: FontWeight.w600,
         letterSpacing: 0.15),
-    //SubHeading
-    displayMedium: GoogleFonts.sourceSansPro(
+    titleMedium: GoogleFonts.sourceSansPro(
       color: Color.fromARGB(252, 2, 2, 2),
       fontSize: 14.0,
       fontWeight: FontWeight.w600,
     ),
+    titleSmall: GoogleFonts.sourceSansPro(
+      color: Color.fromARGB(252, 2, 2, 2),
+      fontSize: 12.0,
+      fontWeight: FontWeight.w400,
+    ),
+    labelLarge: GoogleFonts.sourceSansPro(
+      color: Color.fromARGB(252, 2, 2, 2),
+      fontSize: 10.0,
+      fontWeight: FontWeight.w400,
+    ),
+    labelMedium: GoogleFonts.sourceSansPro(
+      color: Color.fromARGB(252, 2, 2, 2),
+      fontSize: 8.0,
+      fontWeight: FontWeight.w400,
+    ),
     //BODY
-    bodyLarge: GoogleFonts.sourceSansPro(
+    bodyLarge: GoogleFonts.gelasio(
       color: Color.fromARGB(252, 2, 2, 2),
       fontSize: 16.0,
     ),
-    //INPUT-TEXT
-    bodyMedium: GoogleFonts.sourceSansPro(
-        color: Color.fromARGB(252, 2, 2, 2),
-        fontSize: 14.0,
-        letterSpacing: 0.4),
   ),
   inputDecorationTheme: InputDecorationTheme(
     labelStyle: TextStyle(
